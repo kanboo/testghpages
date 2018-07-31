5bc7fa381ca06c29dc5998e47c0d6ac396eb92a1
kanboo <wang.kanboo@gmail.com>
2018-07-31 17:56:14 +0800

    del

--- dist/static/js/0.b9225f0a84fa6dc2aef2.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],{mt7k:function(e,t){},zv4U:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"index"},[this._v("\r\n  Index12312312\r\n")])},staticRenderFns:[]};var i=n("VU/8")({name:"index",components:{},data:function(){return{title:"Vue - Porject"}}},r,!1,function(e){n("mt7k")},"data-v-01b55982",null);t.default=i.exports}});
-//# sourceMappingURL=0.b9225f0a84fa6dc2aef2.js.map
